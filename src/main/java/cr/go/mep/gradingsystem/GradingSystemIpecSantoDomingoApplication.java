ed1a2c917a8fe6d14de9f429a6fdd610016d47db
fbnmlnss <fabimilanes@gmail.com>
2024-07-01 12:38:08 -0600

    first commit

---
+++ src/main/java/cr/go/mep/gradingsystem/GradingSystemIpecSantoDomingoApplication.java
@@ -0,0 +1,13 @@
+package cr.go.mep.gradingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradingSystemIpecSantoDomingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradingSystemIpecSantoDomingoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cr/go/mep/gradingsystem/GradingSystemIpecSantoDomingoApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.go.mep.gradingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradingSystemIpecSantoDomingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
