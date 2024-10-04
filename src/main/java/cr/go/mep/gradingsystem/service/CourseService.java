a1abbf9cebda83a5859cae4e5267e3655d25e408
fbnmlnss <fabimilanes@gmail.com>
2024-10-03 19:44:58 -0600

    feat: implement functionality to return all courses for instructor and add students to a course

--- src/main/java/cr/go/mep/gradingsystem/service/CourseService.java
+++ src/main/java/cr/go/mep/gradingsystem/service/CourseService.java
@@ -3,45 +3,59 @@ package cr.go.mep.gradingsystem.service;
 import cr.go.mep.gradingsystem.dto.CreateCourseRequest;
 import cr.go.mep.gradingsystem.model.Course;
 import cr.go.mep.gradingsystem.model.Instructor;
+import cr.go.mep.gradingsystem.model.Student;
 import cr.go.mep.gradingsystem.repository.CourseRepository;
 import cr.go.mep.gradingsystem.repository.InstructorRepository;
+import cr.go.mep.gradingsystem.repository.StudentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.source.InvalidConfigurationPropertyValueException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class CourseService {
     private final CourseRepository courseRepository;
     private final InstructorRepository instructorRepository;
+    private final StudentRepository studentRepository;
 
     public Long createCourse(CreateCourseRequest createCourseRequest) {
-        Optional<Instructor> optionalInstructor = this.instructorRepository.findById(createCourseRequest.instructorId());
+        Instructor instructor = this.instructorRepository.findById(createCourseRequest.instructorId())
+                .orElseThrow(() -> new InvalidConfigurationPropertyValueException(
+                        "instructor id",
+                        createCourseRequest.instructorId(),
+                        "resource does not exist"));
 
-        if (optionalInstructor.isPresent()) {
-            Instructor instructor = optionalInstructor.get();
+        Course newCourse = new Course(
+                createCourseRequest.type(),
+                createCourseRequest.name(),
+                instructor,
+                createCourseRequest.startDate(),
+                createCourseRequest.endDate(),
+                createCourseRequest.maxCapacity());
 
-            Course newCourse = new Course(
-                    createCourseRequest.type(),
-                    createCourseRequest.name(),
-                    instructor,
-                    createCourseRequest.startDate(),
-                    createCourseRequest.endDate(),
-                    createCourseRequest.maxCapacity());
-
-            return this.courseRepository.save(newCourse).getId();
-        } else {
-            throw new InvalidConfigurationPropertyValueException(
-                    "instructor id",
-                    createCourseRequest.instructorId(),
-                    "resource does not exist");
-        }
+        return this.courseRepository.save(newCourse).getId();
     }
 
     public List<Course> getAllCourses() {
         return this.courseRepository.findAll();
+    }
+
+    public List<Course> getAllCoursesByInstructorId(Long instructorId) {
+        return this.courseRepository.findAllByInstructorId(instructorId);
+    }
+
+    public Long addStudentsToCourse(Long courseId, List<Long> studentIds) {
+        Course course = this.courseRepository.findById(courseId)
+                .orElseThrow(() -> new InvalidConfigurationPropertyValueException(
+                        "course id",
+                        courseId,
+                        "resource does not exist"));
+
+        List<Student> students = this.studentRepository.findAllById(studentIds);
+        course.getStudents().addAll(students);
+
+        return this.courseRepository.save(course).getId();
     }
 }
